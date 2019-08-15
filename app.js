23f099fcebfb2bfc29ed3098499dc4f031931232
Steve Willson <steve.willson@gmail.com>
2019-08-14 20:01:07 -0700

    modify structure to include middleware in app.js, refactor logic for hb_cal.ejs

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var formidableMiddleware = require('express-formidable');
+
+var moment = require('moment');
 
 var monk = require('monk');
 var db = monk('localhost:27017/hb_cal');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var importRouter = require('./routes/import');
 
 var app = express();
+app.use(formidableMiddleware());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +22,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,7 +33,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/import', importRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
-var moment = require('moment');
-var cookieParser = require('cookie-parser');
-var ICAL = require('ical.js');
-var formidableMiddleware = require('express-formidable');
-var fs = require('fs');
-var util = require('util');
-
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+//var cookieParser = require('cookie-parser');
 
-router.use(formidableMiddleware());
-router.use(cookieParser());
+//router.use(cookieParser());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,25 +20,16 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST to Set Begin and End of Calendar Display */
+//app.post('/', function (req, res) {
 router.post('/', function(req, res) {
 
-	console.log("TEST");
-	var form = new formidableMiddleware.IncomingForm();
-	form.parse(req, function (err, fields, files) {
-		console.log(fields);
-
-	});
-
-  // Get our form values. These rely on the 'name' attributes
-
-	/*
-  var beginDate = req.body.datebegin;
-  var endDate = req.body.dateend;
-  var cell_width = req.body.cell_width;
+  // the req.fields object contains the POST values
+  var beginDate = req.fields.datebegin;
+  var endDate = req.fields.dateend;
+  var cell_width = req.fields.cell_width;
   res.cookie("beginDate", beginDate, {expire : new Date() + 9999});
   res.cookie("endDate", endDate, {expire : new Date() + 9999});
   res.cookie("cell_width", cell_width, {expire : new Date() + 9999});
-	*/
 
   // Forward to index page
   res.redirect("/");
@@ -69,83 +54,6 @@ router.get('/transfer', function(req, res) {
   });
 });
 
-/* POST import page. */
-router.post('/import', function(req, res) {
-
-	if (Object.keys(req.files).length == 0) {
-		return res.status(400).send('No files were uploaded.');
-	}
-
-  // Set our internal DB variable
-  var db = req.db;
-	// Set our collection
-	var collection = db.get('usercollection');
-
-	// The name of the input field (i.e. "importfile") is used to retrieve the uploaded file
-	let importfile = req.files.importfile;
-
-  fs.readFile(importfile.path, "utf8",  function (err, data) {
-
-		// read and parse the uploaded .ics file 
-		var jcalData = new ICAL.parse(data);
-
-		// determine the calendar name, for google calendar, it is stored at a specific location
-		var calname = '';
-		if ( jcalData[1][4][0] == "x-wr-calname" ) {
-			calname = jcalData[1][4][3];
-		} else {
-			calname = "Test Cal"; 
-		}
-
-		var eventCategory = calname;
-		var eventTitle;
-		var eventType;
-		var eventStartDate;
-		var eventEndDate;
-
-		// get the component layer, useful for parsing events
-		var vcal = new ICAL.Component(jcalData);
-		var vevents = vcal.getAllSubcomponents("vevent");
-
-		// iterate through all events to add them to the database
-		for (var venv = 0; venv < vevents.length; venv++){
-		  var event = new ICAL.Event(vevents[venv]);
-      try {
-
-				// assign the event summary as the Title
-				eventTitle = event.summary;
-
-				// hardcoded in eventType at this time
-				eventType = "Training";
-
-				// convert the dates to a moment object
-				var startdate = new moment(event.startDate.toJSDate());
-				var enddate = new moment(event.endDate.toJSDate());
-				eventStartDate = startdate.format("YYYY-MM-DD");
-				eventEndDate = enddate.format("YYYY-MM-DD");
-
-				// Submit to the DB
-				collection.insert({
-					"category" : eventCategory,
-					"event" : eventTitle,
-					"eventtype" : eventType,
-					"date_start" : eventStartDate,
-					"date_end" : eventEndDate
-				}, function (err, doc) {
-					if (err) {
-						// If it failed, return error
-						res.send("There was a problem adding the information to the database.");
-					}
-				});
-			}
-      catch {
-				console.log("Couldn't insert event, malformed?");
-			}
-		}
-		// forward to success page
-		res.redirect("/");
-  });
-});
 
 /* GET New Event page. */
 router.get('/newevent', function(req, res) {
@@ -165,15 +73,14 @@ router.post('/newevent', function(req, res) {
   var eventType = "";
   var eventStartDate = "";
   var eventEndDate = "";
- 
- 	/*	
-  // Get our form values. These rely on the 'name' attributes
-  var eventCategory = req.body.eventcategory;
-  var eventTitle = req.body.eventname;
-  var eventType = req.body.eventtype;
-  var eventStartDate = req.body.date_start;
-  var eventEndDate = req.body.date_end;
-	*/
+ 		
+  // Get the form values. These rely on the 'name' attributes
+	// using express-formidable, the POST values are in the req.fields object
+  var eventCategory = req.fields.eventcategory;
+  var eventTitle = req.fields.eventname;
+  var eventType = req.fields.eventtype;
+  var eventStartDate = req.fields.date_start;
+  var eventEndDate = req.fields.date_end;
 
   // Submit to the DB
   collection.insert({
@@ -200,12 +107,10 @@ router.post('/delevent', function(req, res) {
   // Set our internal DB variable
   var db = req.db;
 
-
   var documentid = "";
-  /*
+
   // Get our form values. These rely on the 'name' attributes
-  var documentid = req.body.documentid;
-	*/
+  var documentid = req.fields.documentid;
 
   // Set our collection
   var collection = db.get('usercollection');
